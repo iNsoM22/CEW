cb6179bfae5f60e878c278890861aa06f756415c
Asta <s.a.moizkhan988@gmail.com>
2023-10-23 00:32:22 +0500

    Create ex4.c

---
+++ Lab1/ex4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void decimalToBinary(int num);
+
+int main() {
+   int num;
+   printf("Enter the number to convert it to binary: ");
+   scanf("%d", &num);
+   decimalToBinary(num);
+
+   return 0;
+}
+
+decimalToBinary(num) {
+    char binaryVal[64];
+    int i = 0;
+    if (num == 0) {
+        printf("Binary: 0\n");
+        return;
+    }
+
+    while (num > 0) {
+        binaryVal[i] = (num % 2) + '0';
+        num = num / 2;
+        i++;
+    }
+
+    printf("Binary: ");
+    for (i; i >= 0; i--) {
+        printf("%c", binaryVal[i]);
+    }
+}
