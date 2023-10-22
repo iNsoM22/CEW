0ba1cdbbe51d22f559d28986b6d0d3f2ed2b49c5
Asta <s.a.moizkhan988@gmail.com>
2023-10-22 23:37:53 +0500

    Create task1.c

---
+++ Lab1/task1.c
@@ -0,0 +1,17 @@
+// Takes a number from the user as input, and print its table if the number is positive.
+
+#include <stdio.h>
+
+void printTable(int num);
+
+int main() {
+   int num;
+   printf("Enter a number: ");
+   scanf("%d", &num);
+   num > 0 ? printTable(num) : printf("Number is negative.");
+    return 0;
+}
+
+printTable(num){
+    for(int i=1; i<=10; i++) printf("%d x %d = %d \n", num, i, num*i);
+}
