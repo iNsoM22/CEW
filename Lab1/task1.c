444fd7b619f50bc577740e31bf7acd5aad9761b2
Asta <s.a.moizkhan988@gmail.com>
2023-10-23 00:13:00 +0500

    Update task1.c

--- Lab1/task1.c
+++ Lab1/task1.c
@@ -9,9 +9,9 @@ int main() {
    printf("Enter a number: ");
    scanf("%d", &num);
    num > 0 ? printTable(num) : printf("Number is negative.");
-    return 0;
+   return 0;
 }
 
-printTable(num){
+printTable(num) {
     for(int i=1; i<=10; i++) printf("%d x %d = %d \n", num, i, num*i);
 }
