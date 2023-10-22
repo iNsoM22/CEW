cbc5938cb0d1ed09a65f935a4a3b316a5e4e1c41
Asta <s.a.moizkhan988@gmail.com>
2023-10-23 00:02:32 +0500

    Create ex1.c

---
+++ Lab1/ex1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int id, amount;
+    float workhours, salary;
+    printf("Enter your ID:");
+    scanf("%d",& id);
+    printf("\nEnter the total hours you worked:");
+    scanf("%f", &workhours);
+    printf("\nEnter your hourly wage:");
+    scanf("%d", &amount);
+    salary = workhours*amount; 
+    printf("\nThe ID of employee is %d", id);
+    printf("\nAnd His Salary is %.2f", salary);
+    
+}    
