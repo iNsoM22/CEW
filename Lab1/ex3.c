ff9f184c3c4eba860b2c93bedf870040ce95f115
Asta <s.a.moizkhan988@gmail.com>
2023-10-23 00:17:10 +0500

    Update ex3.c

--- Lab1/ex3.c
+++ Lab1/ex3.c
@@ -1,24 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main() {
     float height;
-    printf("Enter your height in centimeters:\n");
-    scanf("%f",&height);
-    if(height<150){
-        printf("Dwarf");
-         
-    }
-    else if(height=150){
-        printf("Average");
-     
-    }
-    else if(height>165){
-        printf("Tall"); 
-    
-    }
-    else{
-        printf("Please enter the correct height"); 
-    
-    }          
+    printf("Enter your height in centimeters: /n");
+    scanf("%f", &height);
+    if(height < 150) printf("Dwarf");
+    else if(height = 150) printf("Average");
+    else if(height > 165) printf("Tall"); 
+    else printf("Please enter the correct height"); 
+    return 0;
 }    
