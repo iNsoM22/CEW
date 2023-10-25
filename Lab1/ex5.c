39ca66ccbbd76a8a8cbd7ae3abecc7db9a254993
Asta <s.a.moizkhan988@gmail.com>
2023-10-25 10:06:12 +0500

    Update ex5.c
    changed the approach from looping to recursion.

--- Lab1/ex5.c
+++ Lab1/ex5.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printFib(int terms);
+int printFib(int terms) {
+    if(terms <= 1)  return 1;
+    return printFib(terms - 1) + printFib(terms - 2);
+}
 
 int main() {
     int terms;
     printf("Enter the number of terms, you want of fibonacci series: ");
     scanf("%d", &terms);
-    terms > 2 ? printFib(terms) : printf("Invalid Input.");
+    printf("The %d terms of Fibonacci series are : 0 ", terms);
+    for(int i = 0; i < terms - 1; i++){
+        printf("%d ", printFib(i));
+    }
     return 0;
 }
 
-printFib(terms) {
-    int a, b, c;
-    a = 0;
-    b = 1;
-    printf("The %d number of terms of Fibonacci series are : 0 1 ", terms);
-    for(int i=1; i <= terms-2; i++){
-        c = a + b;
-        a = b;
-        b = c;
-        printf("%d ", c);
-    }
-}
