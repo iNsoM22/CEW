c0f90486dc7a251103fcea58085168d83461d991
Asta <s.a.moizkhan988@gmail.com>
2023-10-30 00:07:19 +0500

    Update ex2.c
    Added a new approach to the problem.

--- Lab2/ex2.c
+++ Lab2/ex2.c
@@ -11,6 +11,15 @@ int main() {
         for(int k=1; k<=i; k++) printf("* ");
         printf("\n"); 
     }
-
+// 2nd Approach
+//     for(int i=0; i<=n; i++){
+//         for(int j=0; j<=n; j++){
+//             if(j<n-i) printf(" ");
+//             else printf("* ");
+//         }
+//         printf("\n");
+//     }
+    
     return 0;
 }
+
