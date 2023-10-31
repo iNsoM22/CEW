55791e9164c4dd391d49db4071a7bee4bfbaa86f
Asta <s.a.moizkhan988@gmail.com>
2023-10-31 21:47:14 +0500

    Update ex2.c

--- Lab2/ex2.c
+++ Lab2/ex2.c
@@ -12,7 +12,7 @@ int main() {
         printf("\n"); 
     }
 // 2nd Approach
-//     for(int i=0; i<=n; i++){
+//     for(int i=0; i<n; i++){
 //         for(int j=0; j<=n; j++){
 //             if(j<n-i) printf(" ");
 //             else printf("* ");
